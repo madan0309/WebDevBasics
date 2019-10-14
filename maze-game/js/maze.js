39cf3e8a034d69649a8052ee1094de93c88b5973
Madan Kuruba <mkuruba@ah-mkuruba-maci.dhcp.mathworks.com>
2019-10-14 16:41:07 -0400

    responsive size of canvas

--- maze-game/js/maze.js
+++ maze-game/js/maze.js
@@ -94,18 +94,20 @@ class Maze {
         var ctx = canvas.getContext("2d");
         let i = 0, j=0;
         ctx.lineCap = "square";
-        let cellsize = canvas.width/this.width;
+        let cellsizex = canvas.width/this.width;
+        let cellsizey = canvas.height/this.width;
         ctx.beginPath();
+        ctx.lineWidth = 1;
         for(i; i< this.width; i++) {
             for(j=0; j< this.height; j++) {
                 if(!this.maze[i][j].right) {
-                    ctx.moveTo((j+1)*cellsize, (i)*cellsize);
-                    ctx.lineTo((j+1)*cellsize, (i+1)*cellsize);
+                    ctx.moveTo((j+1)*cellsizex, (i)*cellsizey);
+                    ctx.lineTo((j+1)*cellsizex, (i+1)*cellsizey);
                     ctx.stroke();
                 }
                 if(!this.maze[i][j].bottom) {
-                    ctx.moveTo((j)*cellsize, (i+1)*cellsize);
-                    ctx.lineTo((j+1)*cellsize, (i+1)*cellsize);
+                    ctx.moveTo((j)*cellsizex, (i+1)*cellsizey);
+                    ctx.lineTo((j+1)*cellsizex, (i+1)*cellsizey);
                     ctx.stroke();
                 }
             }
@@ -170,20 +172,22 @@ class Player {
 
     setPlayer(x, y) {
         let canvas = document.getElementById("myCanvas");
-        let cellsize = canvas.width/this.width;
+        let cellsizex = canvas.width/this.width;
+        let cellsizey = canvas.height/this.width;
+        let mincellsize = Math.min(cellsizex, cellsizey);
         let ctx = canvas.getContext("2d");
-        ctx.clearRect(this.y* cellsize +0.2*cellsize, this.x*cellsize + 0.2*cellsize, 
-                            0.6*cellsize, 0.6*cellsize);
+        ctx.clearRect(this.y* cellsizex +0.2*mincellsize, this.x*cellsizey + 0.2*mincellsize, 
+                            0.6*cellsizex, 0.6*cellsizey);
 
         this.x = x;
         this.y = y;
-        let xoffset = (y + 0.5) * (cellsize);
-        let yoffset = (x + 0.5) * (cellsize);
+        let xoffset = (y + 0.5) * (cellsizex);
+        let yoffset = (x + 0.5) * (cellsizey);
 
         
         ctx.beginPath();
         ctx.fillStyle = 'green';
-        ctx.arc(xoffset, yoffset, cellsize/4, 0, 2 * Math.PI, true);
+        ctx.arc(xoffset, yoffset, mincellsize/4, 0, 2 * Math.PI, true);
         ctx.fill();
         // ctx.lineWidth = 1;
         // ctx.strokeStyle = '#003300';
@@ -193,13 +197,15 @@ class Player {
     _setTarget() {
         let canvas = document.getElementById("myCanvas");
         let ctx = canvas.getContext("2d");
-        let cellsize = canvas.width/this.width;
-        let xoffset = (this.width - 0.5) * (cellsize);
-        let yoffset = (this.height - 0.5) * (cellsize);
+        let cellsizex = canvas.width/this.width;
+        let cellsizey = canvas.height/this.width;
+        let mincellsize = Math.min(cellsizex, cellsizey);
+        let xoffset = (this.width - 0.5) * (cellsizex);
+        let yoffset = (this.height - 0.5) * (cellsizey);
         
         ctx.beginPath();
         ctx.fillStyle = 'red';
-        ctx.arc(xoffset, yoffset, cellsize/4, 0, 2 * Math.PI, true);
+        ctx.arc(xoffset, yoffset, mincellsize/4, 0, 2 * Math.PI, true);
         ctx.fill();
         // ctx.lineWidth = 1;
         // ctx.strokeStyle = '#003300';
@@ -254,15 +260,15 @@ function setupTheGame() {
     // easy - 6 x 6
     let mazeCategory  = {
         "1" : {
-            "size" : 6,
-            "boundaryprob": 0.5
+            "size" : 8,
+            "boundaryprob": 0.6
         },
         "2" : {
             "size" : 14,
-            "boundaryprob": 0.5
+            "boundaryprob": 0.6
         },
         "3" : {
-            "size" : 20,
+            "size" : 18,
             "boundaryprob": 0.5
         }
     }
@@ -272,9 +278,19 @@ function setupTheGame() {
     p = new Player(size, size);
 }
 
+function setSizeOfCanvas() {
+    let canvas = document.getElementById("myCanvas");
+    let canvasdiv = document.getElementById("canvas-container");
+    canvas.setAttribute("width", canvasdiv.offsetWidth);
+    canvas.setAttribute("height", canvasdiv.offsetHeight);
+    setupTheGame();
+}
 
 window.onload = function() {
+    document.getElementsByClassName("outer")[0].style.maxHeight = window.innerHeight;
+    setSizeOfCanvas();
     setupTheGame();
+    window.addEventListener("resize", setSizeOfCanvas);
     window.addEventListener('keydown', function(evt) {
         // Arrow UP
         if(evt.keyCode == 38) {
